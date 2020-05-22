e8ccf83199d24228539f141643c83b0c1659decf
Saz <saziye@gmail.com>
2020-05-22 13:41:50 +0300

    Avoiding Stale References

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -7,35 +7,34 @@ import {
 
 export default (shouldTrack, callback) => {
     const [err, setErr] = useState(null); //hata olup olmadığını görüntülemek için
-    const [subscriber, setSubscriber] = useState(null);
-
-    const startWatching = async () => {
-        try {
-            await requestPermissionsAsync();
-            const sub = await watchPositionAsync ({
-                accuracy: Accuracy.BestForNavigation,
-                timeInterval: 1000,
-                distanceInterval: 10
-            }, 
-            /*(location)=> {
-                addLocation(location);
-                console.log(location);   
-            }*/
-                callback
-            );
-            setSubscriber(sub);
-        } catch (error)
-        { //eğer kullanıcı reddettiyse otomatik olarak catch'e düşer
-            setErr(error) //err state'inin değerini güncelleriz.
-        }
-    };
 
     useEffect(()=> {
+
+        let subscriber;
+
+        const startWatching = async () => {
+            try {
+                await requestPermissionsAsync();
+                subscriber = await watchPositionAsync ({
+                    accuracy: Accuracy.BestForNavigation,
+                    timeInterval: 1000,
+                    distanceInterval: 10
+                }, 
+                    callback
+                );
+            } catch (error)
+            { //eğer kullanıcı reddettiyse otomatik olarak catch'e düşer
+                setErr(error) //err state'inin değerini güncelleriz.
+            }
+        };
+
         if (shouldTrack) {
             startWatching();
         } else {
-            subscriber.remove();
-            setSubscriber(null);
+            if(subscriber) {
+                subscriber.remove();
+            }
+            subscriber = null;
         }
 
         return() => {
