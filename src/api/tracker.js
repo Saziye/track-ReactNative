b118a4d1225abe1bbac166249aeff9d4e11002b2
Saz <saziye@gmail.com>
2020-05-22 18:43:53 +0300

    Authomatic Authentication (add token on request instance)

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,6 +1,22 @@
 import axios from 'axios';
+import {AsyncStorage} from 'react-native';
 
-export default axios.create({
-    baseURL: 'http://7a276ca4.ngrok.io'
+const instance = axios.create({
+    baseURL: 'http://9e03ae29.ngrok.io'
 });
+
+instance.interceptors.request.use(
+    async (config) => {
+        const token = await AsyncStorage.getItem('token');
+        if(token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (err) => {
+        return Promise.reject(err);
+    }
+);
+
+export default instance;
 
