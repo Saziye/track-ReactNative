3232b8ac022af6f3c2d48f9db879ddc09cbe5cb3
Saz <saziye@gmail.com>
2020-05-22 19:56:53 +0300

    Fetching Created Tracks

--- src/context/TrackContext.js
+++ src/context/TrackContext.js
@@ -3,15 +3,18 @@ import trackerApi from '../api/tracker';
 
 const trackReducer = (state, action) => {
     switch (action.type) {
-        case value:
-            return; 
+        case 'fetch_tracks':
+            return action.payload;
         default:
             return state;
     }
 };
 
 //Backend API'da kayıtlı tüm track'leri getirmesi için action function
-const fetchTracks = dispatch => () => {};
+const fetchTracks = dispatch => async() => {
+    const response = await trackerApi.get('/tracks');
+    dispatch({type: 'fetch_tracks', payload: response.data});
+};
 
 //name ve locations property'lerini alıp kaydetmesi için backend API'a göndereceğimiz action function
 const createTrack = dispatch => async(name, locations) => {
