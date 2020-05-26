b47d3f87730e236d298c2058a33dd6c9176a93e6
Saz <saziye@gmail.com>
2020-05-26 19:26:35 +0300

    Fixing Odds and Ends

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react';
-import { StyleSheet, Text, FlatList, TouchableOpacity} from 'react-native';
+import { StyleSheet, FlatList, TouchableOpacity} from 'react-native';
 import {NavigationEvents} from 'react-navigation';
 import {ListItem} from 'react-native-elements';
 import {Context as TrackContext} from '../context/TrackContext';
@@ -11,7 +11,6 @@ const TrackListScreen = ({navigation}) => {
     return(
         <>
             <NavigationEvents onWillFocus={fetchTracks} />
-            <Text style={{fontSize:48}}>TrackListScreen</Text>
             <FlatList 
                 data= {state}
                 keyExtractor = {item => item._id}
@@ -30,6 +29,10 @@ const TrackListScreen = ({navigation}) => {
         </>
        
     ); 
+};
+
+TrackListScreen.navigationOptions = {
+    title: 'Tracks'
 };
 
 const styles = StyleSheet.create({});
